03f730bd8459480a1513f9de675db8e901f33265
Swati002 <swatigoel37@gmail.com>
2023-04-18 20:12:40 +0530

    Added

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,22 +2,32 @@ import React from 'react'
 import './Card.css'
 import Data from './Data'
 
-
 export default function Card(props) {
   return (
     <>
-    <div className='container'>
+    {/* <div className='container'>
         <div className='box'>
-        <div className='content'>
-            <img className='srcimage' src={props.banner} alt="banner"></img>
-            <div className='text'>
+          <div className='content'>
+              <img className='srcimage' src={props.banner} alt="banner"></img>
+              <div className='text'>
+                <p className='movie-category'>{props.category}</p>
+                <p className='movie-name'>{props.title}</p>
+                <p><a href={props.link} target="_blank"><button>Watch Now</button></a></p>
+              </div>
+          </div>
+        </div>
+    </div> */}
+
+      <div className='container'>
+        <div className='box'>
+          <img className='srcimage' src={props.banner} alt="banner"></img>
+          <div className='text'>
             <p className='movie-category'>{props.category}</p>
             <p className='movie-name'>{props.title}</p>
             <p><a href={props.link} target="_blank"><button>Watch Now</button></a></p>
-            </div>
+          </div>
         </div>
-        </div>
-    </div>
+      </div>
     </>
   )
 }
--- src/Data.js
+++ src/Data.js
@@ -1,42 +1,44 @@
 const Data=[{
     id:1,
     title:"WEDNESDAY",
-    banner:"https://s.yimg.com/ny/api/res/1.2/ne5lVXWlWJ8C6f6Bvoz9_Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/en/deadline.com/fb23e989d8acd53c7b44d0df737cc5f8",
+    // banner:"https://s.yimg.com/ny/api/res/1.2/ne5lVXWlWJ8C6f6Bvoz9_Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/en/deadline.com/fb23e989d8acd53c7b44d0df737cc5f8",
+    banner: ".././images/id1.jpg",
     category: "NETFLIX ORIGINAL SERIES",
     link:"https://www.netflix.com/in/title/81231974",
 
 },{
     id:2,
     title:"MY HOLO LOVE",
-    banner:"https://upload.wikimedia.org/wikipedia/en/4/42/My_Holo_Love.jpg",
+    // banner:"https://upload.wikimedia.org/wikipedia/en/4/42/My_Holo_Love.jpg",
+    banner: ".././images/id2.jpg",
     category: "NETFLIX ORIGINAL SERIES",
     link:"https://www.netflix.com/in/title/81008021?source=35",
 
 },{
     id:3,
     title:"SQUID GAMES",
-    banner:"https://www.digitalphablet.com/wp-content/uploads/2021/10/squid-game.jpg.webp",
+    banner:".././images/id3.webp",
     category: "NETFLIX ORIGINAL SERIES",
     link:"https://www.netflix.com/in/title/81040344?source=35",
 
 },{
     id:4,
     title:"PEAKY BLINDERS",
-    banner:"https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/02/Peaky-Blinders-Cast-and-Character-Guide.jpg",
+    banner:".././images/id4.avif",
     category: "NETFLIX ORIGINAL SERIES",
     link:"https://www.netflix.com/in/title/80002479?source=35",
 
 },{
     id:5,
     title:"VAMPIRE DIARIES",
-    banner:"https://ntvb.tmsimg.com/assets/p10700229_b_h10_aa.jpg?w=960&h=540",
+    banner:".././images/id5.jpg",
     category: "AMAZON ORIGINAL SERIES",
     link:"https://www.netflix.com/in/title/70143860?source=35",
 
 },{
     id:6,
     title:"SHERLOCK HOLMES",
-    banner:"https://resizing.flixster.com/spmdyAWNLVI1XIwiAzuZqJU5gAw=/206x305/v2/https://flxt.tmsimg.com/assets/p8992561_b_v8_aa.jpg",
+    banner:".././images/id6.jpg",
     category: "NETFLIX ORIGINAL SERIES",
     link:"https://www.netflix.com/in/title/70110558#:~:text=2009%20%7C%20Maturity%20Rating%3AU%2F,Watch%20all%20you%20want.",
 
